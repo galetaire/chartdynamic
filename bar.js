a315594bb4d5389e5f3fd1aa8364c4c47e0a4384
iGaliano <84567989+galetaire@users.noreply.github.com>
2023-01-11 14:18:48 +0100

    update

---
+++ bar.js
@@ -0,0 +1,25 @@
+var ctx = document.getElementById('bar').getContext('2d');
+var chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+        datasets: [
+            {
+                label: 'Data Set 1',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1
+            }
+        ]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
